ebb04c328c1ba4246571cd1653a8964ef9bf0f6d
Ivan Suftin <isuftin@usgs.gov>
2015-10-10 10:19:25 -0700

    Cleaning up Commons for DSAS

--- dsas-commons/src/main/java/gov/usgs/cida/utilities/communication/CSWHandler.java
+++ dsas-commons/src/main/java/gov/usgs/cida/utilities/communication/CSWHandler.java
@@ -1,18 +1,11 @@
 package gov.usgs.cida.utilities.communication;
 
-import java.io.ByteArrayInputStream;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.AbstractHttpEntity;
-import org.apache.http.entity.InputStreamEntity;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
 
