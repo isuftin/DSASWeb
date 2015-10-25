8ff6b010349c7319ba116cfcb070066eeb877859
Ivan Suftin <isuftin@usgs.gov>
2015-10-24 22:04:23 -0500

    Fixing up jasmine tests

--- dsas-web-ui/src/test/js/specs/controller/AppRouterSpec.js
+++ dsas-web-ui/src/test/js/specs/controller/AppRouterSpec.js
@@ -10,15 +10,15 @@ define([
 	"use strict";
 	describe("Router", function () {
 		beforeEach(function () {
-			var $div = $('<div />')
+			this.$div = $('<div />')
 					.prop({
 						id: "page-content-container",
 						role: "application"
 					}).addClass("container-fluid");
-			$('body').append($div);
+			$('body').append(this.$div);
 			
 			this.server = sinon.fakeServer.create();
-			this.server.respondWith("GET", "service/session", [
+			this.server.respondWith("GET", /service\/session*/, [
 				200,
 				{"Content-Type": "application/json"},
 				JSON.stringify({"success":"true"})
@@ -37,12 +37,15 @@ define([
 		});
 
 		it("does not fire for unknown paths", function () {
-			this.router.navigate("unknown", true);
+			this.router.navigate("unknown", {trigger : true});
+			this.server.respond();
 			expect(this.routeSpy.notCalled).toBeTruthy();
 		});
+		
 
 		afterEach(function () {
-			$("#page-content-container").remove();
+			this.$div.remove();
+			this.server.restore();
 		});
 	});
 });
