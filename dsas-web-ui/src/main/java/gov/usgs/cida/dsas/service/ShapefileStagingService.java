d7f3d6193c5086570597ecea79866120d4c4ec0d
Ivan Suftin <isuftin@usgs.gov>
2015-11-18 17:39:32 -0600

    Fixed imports

--- dsas-web-ui/src/main/java/gov/usgs/cida/dsas/service/ShapefileStagingService.java
+++ dsas-web-ui/src/main/java/gov/usgs/cida/dsas/service/ShapefileStagingService.java
@@ -16,13 +16,11 @@ import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
-import org.apache.commons.lang.StringUtils;
 import org.slf4j.LoggerFactory;
 
 /**
