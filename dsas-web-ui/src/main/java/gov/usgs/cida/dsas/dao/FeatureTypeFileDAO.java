dcc52973049151213587fc38091612739f707e8b
Suzanne M Larson <smlarson@usgs.gov>
2015-11-24 16:06:20 -0600

    missed in previous merge

---
+++ dsas-web-ui/src/main/java/gov/usgs/cida/dsas/dao/FeatureTypeFileDAO.java
@@ -0,0 +1,75 @@
+package gov.usgs.cida.dsas.dao;
+
+import gov.usgs.cida.dsas.dao.geoserver.GeoserverDAO;
+import gov.usgs.cida.dsas.dao.postgres.PostgresDAO;
+import gov.usgs.cida.dsas.service.util.Property;
+import gov.usgs.cida.dsas.service.util.PropertyUtil;
+import gov.usgs.cida.dsas.shoreline.exception.ShorelineFileFormatException;
+import java.io.File;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import org.geotools.feature.SchemaException;
+import org.opengis.referencing.FactoryException;
+import org.opengis.referencing.operation.TransformException;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author smlarson
+ */
+public abstract class FeatureTypeFileDAO {
+    
+    public static final int DATABASE_PROJECTION = 4326;
+    public static final String DB_SCHEMA_NAME = PropertyUtil.getProperty(Property.DB_SCHEMA_NAME, "public");
+    public static final String[] PROTECTED_WORKSPACES = new String[]{GeoserverDAO.PUBLISHED_WORKSPACE_NAME};
+    protected String JNDI_NAME;
+    protected final PostgresDAO pgDao = new PostgresDAO();
+    private static final org.slf4j.Logger LOGGER = LoggerFactory.getLogger(FeatureTypeFileDAO.class);
+    /**
+     * Retrieves a connection from the database
+     *
+     * @return
+     */
+    protected Connection getConnection() {
+        Connection con = null;
+        try {
+            Context initCtx = new InitialContext();
+            Context envCtx = (Context) initCtx.lookup("java:comp/env");
+            DataSource ds = (DataSource) envCtx.lookup(JNDI_NAME);
+            con = ds.getConnection();
+        } catch (SQLException | NamingException ex) {
+            LOGGER.error("Could not create database connection", ex);
+        }
+        return con;
+    }
+
+    /**
+     * Imports the shoreline file into the database. Returns the name of the
+     * view that holds this shoreline
+     *
+     * @param shorelineFile File that will be used to import into the database
+     * @param columns mapping of file columns to database required columns
+     * @param workspace the unique name of workspace to create or append to
+     * @param EPSGCode the projection code for this shoreline file
+     * @return
+     * @throws
+     * gov.usgs.cida.dsas.shoreline.exception.ShorelineFileFormatException
+     * @throws java.sql.SQLException
+     * @throws javax.naming.NamingException
+     * @throws java.text.ParseException
+     * @throws java.io.IOException
+     * @throws org.geotools.feature.SchemaException
+     * @throws org.opengis.referencing.FactoryException
+     * @throws org.opengis.referencing.operation.TransformException
+     */
+    public abstract String importToDatabase(File shorelineFile, Map<String, String> columns, String workspace, String EPSGCode) throws ShorelineFileFormatException, SQLException, NamingException, NoSuchElementException, ParseException, IOException, SchemaException, TransformException, FactoryException;
+    
+}
