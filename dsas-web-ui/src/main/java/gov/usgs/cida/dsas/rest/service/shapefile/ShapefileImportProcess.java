72c78cb420dd1c05e20da0a9c4f2fe46ff04af4e
Suzanne M Larson <smlarson@usgs.gov>
2015-12-11 11:37:27 -0600

    updated to use the FeatureTypeFile and the FtfTokenExchanger

--- dsas-web-ui/src/main/java/gov/usgs/cida/dsas/rest/service/shapefile/ShapefileImportProcess.java
+++ dsas-web-ui/src/main/java/gov/usgs/cida/dsas/rest/service/shapefile/ShapefileImportProcess.java
@@ -6,6 +6,7 @@ import gov.usgs.cida.dsas.model.DSASProcess;
 import gov.usgs.cida.dsas.model.DSASProcessStatus;
 import gov.usgs.cida.dsas.service.util.TokenFileExchanger;
 import gov.usgs.cida.dsas.featureType.file.FeatureTypeFile;
+import gov.usgs.cida.dsas.featureType.file.TokenFeatureTypeFileExchanger;
 import gov.usgs.cida.dsas.shoreline.file.IShorelineFile;
 import gov.usgs.cida.dsas.shoreline.file.TokenToShorelineFileSingleton;
 import java.io.FileNotFoundException;
@@ -42,30 +43,31 @@ public class ShapefileImportProcess implements Runnable {
 	@Override
 	public void run() {
 		this.process.setStatus(DSASProcessStatus.RUNNING);
-		IShorelineFile shorelineFile = null;
+		//IShorelineFile shorelineFile = null;
+		FeatureTypeFile featureTypeFile = null;
 		try {
 			if (StringUtils.isNotBlank(this.fileToken)) {
 				this.process.setPercentCompleted(1);
 				//TODO: Change IShorelineFile to a more generic Shapefile interface, 
 				// removing the idea of Shorelines from this prcess and push that down
 				// to implementing classes
-				//shorelineFile = TokenFileExchanger.getFile(this.fileToken);
-				shorelineFile = TokenToShorelineFileSingleton.getShorelineFile(this.fileToken);
-				shorelineFile.setDSASProcess(process);
+				//shorelineFile = TokenToShorelineFileSingleton.getShorelineFile(this.fileToken); 
+				featureTypeFile = TokenFeatureTypeFileExchanger.getFeatureTypeFile(this.fileToken);
+				featureTypeFile.setDSASProcess(process);
 				
-				if (!shorelineFile.exists()) {
+				if (!featureTypeFile.exists()) {
 					throw new FileNotFoundException(String.format("File not found for token %s", this.fileToken));
 				}
 				this.process.setPercentCompleted(33);
 				this.process.addProcessInformation(String.format("File found for token %s", this.fileToken));
 				this.process.addProcessInformation("Importing file to database");
 
-				String viewName = shorelineFile.importToDatabase(columns, this.workspace);
+				String viewName = featureTypeFile.importToDatabase(columns, this.workspace);
 				this.process.setPercentCompleted(66);
 				this.process.addProcessInformation("Import to database complete");
 				
 				try {
-					new PostgresDAO().updateWorkspaceLastAccessTime(this.workspace);
+					new PostgresDAO().updateWorkspaceLastAccessTime(this.workspace); 
 					this.process.addProcessInformation("Workspace last access time updated");
 				} catch (SQLException ex) {
 					this.process.addProcessInformation("Workspace last access time could not be updated");
@@ -73,7 +75,7 @@ public class ShapefileImportProcess implements Runnable {
 				
 				this.process.addProcessInformation("Importing to Geoserver");
 
-				shorelineFile.importToGeoserver(viewName, this.workspace);
+				featureTypeFile.importToGeoserver(viewName, this.workspace);
 				this.process.addProcessOutput("layer", viewName);
 				this.process.addProcessOutput("workspace", this.workspace);
 				this.process.setPercentCompleted(100);
@@ -87,8 +89,8 @@ public class ShapefileImportProcess implements Runnable {
 			this.process.setStatus(DSASProcessStatus.TERMINATED);
 			this.process.addProcessInformation(ex.getMessage());
 		} finally {
-			if (shorelineFile != null) {
-				shorelineFile.clear();
+			if (featureTypeFile != null) {
+				featureTypeFile.clear();
 			}
 		}
 	}
